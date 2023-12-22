b8994b0dfdaf43b0d7c0027e7c0b3bcb3b85f969
Ethan k <ethan7klukkert@gmail.com>
2023-12-22 16:59:36 -0500

    Add error handling and authentication

---
+++ app/dashboard/invoices/error.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useEffect } from 'react';
+
+// this is a catch-all error page for the invoices route. It catches database errors, UI errors, etc...
+// the not-found.tsx file is more specific, and takes precedence. It is able to handle specific errors and 
+// be more informative to the user's UI.
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    useEffect(() => {
+        // Optionally log the error to an error reporting service
+        console.error(error);
+    }, [error]);
+
+    return (
+        <main className="flex h-full flex-col items-center justify-center">
+            <h2 className="text-center">Something went wrong!</h2>
+            <button
+                className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+                onClick={
+                    // Attempt to recover by trying to re-render the invoices route
+                    () => reset()
+                }
+            >
+                Try again
+            </button>
+        </main>
+    );
+}
